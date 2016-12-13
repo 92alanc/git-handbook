cadd04ad2d03421377ba3fe704cf62f06e668bd3
alcam92 <alancamargo92@yahoo.co.uk>
2016-12-13 18:40:55 -0200

    Fixed build issue

--- app/src/main/java/com/braincorp/githandbook/activities/MainActivity.java
+++ app/src/main/java/com/braincorp/githandbook/activities/MainActivity.java
@@ -2,7 +2,6 @@ package com.braincorp.githandbook.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,14 +24,13 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.goButton);
-        fab.setOnClickListener(new View.OnClickListener()
+        FloatingActionButton goButton = (FloatingActionButton) findViewById(R.id.goButton);
+        goButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
@@ -40,7 +38,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -48,17 +45,12 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        switch (id)
         {
-            return true;
+            default:
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
