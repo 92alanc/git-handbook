d56c72ad12e301c1f71cf8c808db7c29e3bc917f
alcam92 <alancamargo92@yahoo.co.uk>
2016-12-13 18:37:45 -0200

    Initial commit

---
+++ app/src/main/java/com/braincorp/githandbook/database/DatabaseHelper.java
@@ -0,0 +1,9 @@
+package com.braincorp.githandbook.database;
+
+/**
+ * Database helper class
+ * Created by Alan Camargo - December 2016
+ */
+class DatabaseHelper
+{
+}
---
+++ app/src/main/java/com/braincorp/githandbook/model/Command.java
@@ -0,0 +1,9 @@
+package com.braincorp.githandbook.model;
+
+/**
+ * Command class
+ * Created by Alan Camargo - December 2016
+ */
+public class Command
+{
+}
