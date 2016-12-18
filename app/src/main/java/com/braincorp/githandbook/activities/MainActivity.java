e4ca5b1c06da728b0af7e0cf555b1f9062175531
alcam92 <alancamargo92@yahoo.co.uk>
2016-12-17 23:24:14 -0200

    Added a lot of stuff

--- app/src/main/java/com/braincorp/githandbook/activities/MainActivity.java
+++ app/src/main/java/com/braincorp/githandbook/activities/MainActivity.java
@@ -1,7 +1,13 @@
 package com.braincorp.githandbook.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -48,9 +54,54 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
         switch (id)
         {
+            case R.id.item_about:
+                showAppInfo();
+                break;
+            case R.id.item_reference:
+                openGitWebPage();
+                break;
+            case R.id.item_search:
+                // TODO: search
+                break;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void showAppInfo()
+    {
+        try
+        {
+            PackageManager manager = getPackageManager();
+            PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
+            String version = info.versionName;
+            String title = String.format("%1$s %2$s", getString(R.string.app_name), version);
+            AlertDialog.Builder dialogue = new AlertDialog.Builder(this);
+            dialogue.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    // Do nothing
+                }
+            });
+            dialogue.setTitle(title);
+            dialogue.setMessage(R.string.about_text);
+            dialogue.show();
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            // Damn! Something really wrong happened here
+            e.printStackTrace();
+        }
+    }
+
+    private void openGitWebPage()
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(getString(R.string.git_url)));
+        startActivity(intent);
+    }
+
 }
