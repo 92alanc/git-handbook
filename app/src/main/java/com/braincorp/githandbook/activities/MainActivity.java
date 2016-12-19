db85d169c9daa90061f3a32857fddf7a0087a789
Alan Camargo <alan.camargo@intra.labone.net>
2016-12-19 14:42:17 -0200

    1.0 apparently ready

--- app/src/main/java/com/braincorp/githandbook/activities/MainActivity.java
+++ app/src/main/java/com/braincorp/githandbook/activities/MainActivity.java
@@ -61,9 +61,6 @@ public class MainActivity extends AppCompatActivity
             case R.id.item_reference:
                 openGitWebPage();
                 break;
-            case R.id.item_search:
-                // TODO: search
-                break;
             default:
                 break;
         }
