d731e7104a3d08865e69632c3260ebe7d3a72a68
Alan Camargo <alan.uk.br@gmail.com>
2019-04-23 16:32:57 -0300

    Implement CommandAdapter

---
+++ app/src/main/kotlin/com/braincorp/githandbook/adapter/CommandAdapter.kt
@@ -0,0 +1,34 @@
+package com.braincorp.githandbook.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.braincorp.githandbook.R
+import com.braincorp.githandbook.model.Command
+
+class CommandAdapter : ListAdapter<Command, CommandViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommandViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.item_command, parent, false)
+        return CommandViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CommandViewHolder, position: Int) {
+        val command = getItem(position)
+        holder.bindTo(command)
+    }
+
+    private companion object DiffCallback : DiffUtil.ItemCallback<Command>() {
+        override fun areItemsTheSame(oldItem: Command, newItem: Command): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Command, newItem: Command): Boolean {
+            return oldItem.name == newItem.name
+                    && oldItem.params.contentEquals(newItem.params)
+        }
+    }
+
+}
